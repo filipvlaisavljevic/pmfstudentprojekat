ee671b9e4ade801f785f5d288a6d3e22d9a772af
Filip Vlaisavljevic <filipvlaisavljevic@outlook.com>
2021-07-15 13:11:32 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {Container,Button} from "react-bootstrap";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import HomeComponent from "./components/HomeComponent";
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import LoginComponent from "./components/LoginComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Container>
+          <HeaderComponent/>
+            {/*  Prikaz početne stranice za sve logovane korisnike*/}
+            <Switch>
+                <Route path="/login">
+                    <LoginComponent />
+                </Route>
+                <Route path="/">
+                    <HomeComponent/>
+                </Route>
+            </Switch>
+          <FooterComponent/>
+        </Container>
+      </Router>
   );
 }
 
