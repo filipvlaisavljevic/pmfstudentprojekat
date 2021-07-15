4f9b8cd6066bfe163b3a915106b94f1789907bad
Filip Vlaisavljevic <filipvlaisavljevic@outlook.com>
2021-07-15 14:37:01 +0200

    Homepage proporcije

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,12 +1,19 @@
 import React from "react"
-import {Row,Col} from "react-bootstrap";
+import {Row,Col,Container} from "react-bootstrap";
+import { CaretDownFill } from 'react-bootstrap-icons';
 
 function HomeComponent(){
     return(
-        <Row className={"mt-5 mb-5"}>
-            <Col sm={8}></Col>
-            <Col sm={4}></Col>
-        </Row>
+        <Container>
+            <Row className={"mt-5 mb-5"}>
+                <Col sm={8} className={"banner pt-2 pb-2 okvir-desni"}>
+                    <CaretDownFill/> Nedavne objave ostalih studenata
+                </Col>
+                <Col sm={4} className={"banner pt-2 pb-2"}>
+                    <CaretDownFill/> Dodajte i nove studente
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
