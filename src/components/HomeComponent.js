3b07ad376558cf9d16bf73d3cf4ad92fd5b9d68c
DJReTa <blackiron.old@gmail.com>
2021-07-16 23:53:24 +0200

    padding volume 32

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -10,14 +10,14 @@ function HomeComponent(){
         <div>
             <Container>
                 <Row className={"mb-4"}>
-                    <Col sm={8}>
+                    <Col sm={8} style={{paddingLeft: 0}}>
                         <div className={"mt-5 banner pt-2 pb-2 mb-4"}>
                             <CaretDownFill/> Nedavne objave ostalih studenata
                         </div>
                         <ObjaviPostComponent/>
                         <ObjavaComponent/>
                     </Col>
-                    <Col sm={4}>
+                    <Col sm={4} style={{paddingRight: 0}}>
                         <div className={"mt-5 banner pt-2 pb-2 mb-4"}>
                             <CaretDownFill/> Dodajte i nove studente
                         </div>
