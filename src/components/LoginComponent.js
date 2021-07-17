9f9f00be4840357e70b2c481246e53a6ed0cfacc
DJReTa <blackiron.old@gmail.com>
2021-07-17 16:50:30 +0200

    login i register

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -2,6 +2,7 @@ import React, {useState} from "react"
 import {Form, Button, Alert} from "react-bootstrap";
 import {useForm} from "react-hook-form";
 import axios from "axios";
+import {XCircle} from "react-bootstrap-icons";
 
 axios.defaults.withCredentials = true;
 
@@ -52,7 +53,7 @@ function LoginComponent({postaviSesiju,unistiSesiju}){
         <Form className={"mt-4 mb-4"} onSubmit={handleSubmit(onSubmit)}>
             <Alert variant="danger" show={show} onClose={zatvoriAlert.bind(this)} dismissible>
                 <p className="bez-margine">
-                    {poruka}
+                    <XCircle/> {poruka}
                 </p>
             </Alert>
             <Form.Group className="mb-3">
--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -2,6 +2,8 @@ import React from "react"
 import {Button, Form} from "react-bootstrap";
 import {useForm} from "react-hook-form";
 import axios from "axios";
+import Feedback from "react-bootstrap/Feedback";
+import {XCircle} from "react-bootstrap-icons";
 
 axios.defaults.withCredentials = true;
 
@@ -36,26 +38,45 @@ function RegisterComponent({postaviSesiju,unistiSesiju}){
     }
 
     return(
-        <Form className={"mt-4 mb-4"} onSubmit={handleSubmit(onSubmit)}>
+        <Form className={"mt-4 mb-4"} onSubmit={handleSubmit(onSubmit)} noValidate>
 
             <Form.Group className="mb-3">
                 <Form.Label>Ime</Form.Label>
-                <Form.Control type="text" placeholder="Unesite ime" {...register("ime")}/>
+                <Form.Control type="text" placeholder="Unesite ime"
+                              aria-invalid={errors.ime ? "true" : "false"}
+                              {...register("ime",{
+                    required: "Morate unijeti ime"
+                })}/>
+                {errors.ime && <p className='greska'><XCircle/> {errors.ime.message}</p>}
             </Form.Group>
 
             <Form.Group className="mb-3">
                 <Form.Label>Prezime</Form.Label>
-                <Form.Control type="text" placeholder="Unesite prezime" {...register("prezime")}/>
+                <Form.Control type="text" aria-invalid={errors.prezime ? "true" : "false"} placeholder="Unesite prezime" {...register("prezime",{
+                    required: "Morate unijeti prezime"
+                })}/>
+                {errors.prezime && <p className='greska'><XCircle/> {errors.prezime.message}</p> }
             </Form.Group>
 
             <Form.Group className="mb-3">
                 <Form.Label>Korisničko ime</Form.Label>
-                <Form.Control type="text" placeholder="Unesite željeno korisničko ime" {...register("username")}/>
+                <Form.Control type="text" aria-invalid={errors.username ? "true" : "false"} placeholder="Unesite željeno korisničko ime" {...register("username",{
+                    required: "Morate unijet korisničko ime"
+                })}/>
+                {errors.username && <p className='greska'><XCircle/> {errors.username.message}</p> }
             </Form.Group>
 
             <Form.Group className="mb-3">
                 <Form.Label>Email adresa</Form.Label>
-                <Form.Control type="email" placeholder="Uniste email adresu" {...register("email")}/>
+                <Form.Control type="email" placeholder="Uniste email adresu" aria-invalid={errors.email ? "true" : "false"} {...register("email",{
+                    required: "Morate unijeti mail",
+                    pattern: {
+                        value: /\S+@\S+\.\S+/,
+                        message: "Niste unijeli dobar format"
+                    }
+
+                })}/>
+                {errors.email && <p className='greska'><XCircle/> {errors.email.message}</p>}
                 <Form.Text className="text-muted">
                     Vašu email adresu nećemo dijeliti sa drugima.
                 </Form.Text>
@@ -63,10 +84,25 @@ function RegisterComponent({postaviSesiju,unistiSesiju}){
 
             <Form.Group className="mb-3">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Unesite password" {...register("password")}/>
+                <Form.Control type="password" aria-invalid={errors.password ? 'true' : 'false'} id="pass1" placeholder="Unesite password" {...register("password",{
+                    required: "Morate unijeti šifru",
+                    minLength: {
+                        value: 8,
+                        message: "Šifra mora minimalno 8 karaktera imati"
+                    }
+                })}/>
+                {errors.password && <p className='greska'><XCircle/> {errors.password.message}</p> }
                 <Form.Text className="text-muted">
                     Vaš password nećemo dijeliti sa drugima.
                 </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Potvrdite password</Form.Label>
+                <Form.Control type="password" aria-invalid={errors.password1 ? 'true' : 'false'} placeholder="Potvrdite password" {...register("password1",{
+                    validate: value =>
+                        value === document.getElementById("pass1").value || "Šifre se ne poklapaju"
+                })}/>
+                {errors.password1 && <p className='greska'><XCircle/> {errors.password1.message}</p> }
             </Form.Group>
             <Button variant="primary" type="submit" className={"w-100"}>
                 Registruj se
