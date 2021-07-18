6f4b02988445e558d3f69a59e49ecf5d9dcd99d5
DJReTa <blackiron.old@gmail.com>
2021-07-18 22:18:01 +0200

    napravio sam delete

--- src/components/FullObjavaComponent.js
+++ src/components/FullObjavaComponent.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react"
-import {Card,ListGroup} from "react-bootstrap";
+import {Button, Card, Dropdown, ListGroup, Modal} from "react-bootstrap";
 import {
     ChatDots,
     ChatQuoteFill,
@@ -7,14 +7,44 @@ import {
     ChatSquareText, HandThumbsDownFill,
     HandThumbsUpFill,
     Hash,
-    Messenger
+    Messenger, ThreeDots, Trash
 } from "react-bootstrap-icons";
 import ObjaviPostComponent from "./ObjaviPostComponent";
 import ObjaviKomentarComponent from "./ObjaviKomentarComponent";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 function FullObjavaComponent({objava,handler}){
 
+    const [show, setShow] = useState(false);
+    const [id,setId]=useState(0)
+
+    const handleClose = () => setShow(false);
+    const handleShow = (id) =>{setId(id); setShow(true);}
     const[prikazi,setPrikazi] = useState(false);
+    function obrisiObjavu(){
+        console.log(id)
+        setShow(false);
+        axios.post('https://dwsproject.herokuapp.com/deletePost',{
+            post_id: id
+        })
+            .then((response)=>{
+                if(!response.data.success){
+                    console.log(response.data)
+                }else{
+                    Swal.fire({
+                        title: 'Super!',
+                        text: 'Uspješno ste izbrisali objavu.',
+                        icon: 'success',
+                        confirmButtonText: 'Nastavi dalje'
+                    })
+                    handler()
+                }
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+    }
 
     function postaviPrikaz(){
         setPrikazi(!prikazi);
@@ -25,15 +55,24 @@ function FullObjavaComponent({objava,handler}){
     }
 
 
-
+    /*
     console.log("OBJAVA")
-    console.log(objava)
+    console.log(objava)*/
 
     return(
       <div>
           <Card className={"mb-3"}>
               {/*<Card.Img variant="top" src="https://i.imgur.com/FW4FhbQ.png" className={"d-none d-lg-block"}/>*/}
               <Card.Body>
+                  <Dropdown>
+                      <Dropdown.Toggle className="dugme-dropdown" noCaret>
+                          <ThreeDots/>
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                          <Dropdown.Item style={{color: "#D83A56"}} onClick={handleShow.bind(this,objava.post.id)}><Trash/> Izbrišite objavu</Dropdown.Item>
+                      </Dropdown.Menu>
+                  </Dropdown>
                   <blockquote className="blockquote mb-0">
                       <p>
                           {' '}
@@ -66,6 +105,28 @@ function FullObjavaComponent({objava,handler}){
                   <ObjaviKomentarComponent objava={objava.post} handler={() => handler()}/>
               </div> :
               <div></div>}
+          <Modal
+              show={show}
+              size="lg"
+              aria-labelledby="contained-modal-title-vcenter"
+              centered
+              onHide={handleClose}
+          >
+              <Modal.Body style={{textAlign: "center"}}>
+                  <h4>Izbrisati objavu?</h4>
+                  <p style={{textAlign: "center"}}>
+                      Ukoliko izbrišite objavu, zauvijek će nestati sa Vašeg profila i ne možemo je vratiti.
+                  </p>
+              </Modal.Body>
+              <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                      Otkaži
+                  </Button>
+                  <Button className="dugme-warning" onClick={obrisiObjavu.bind(this)}>
+                      Izbriši
+                  </Button>
+              </Modal.Footer>
+          </Modal>
       </div>
     );
 }
