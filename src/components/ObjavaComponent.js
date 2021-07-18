4e22d4bbb4b46aee6f0815c9a370ed69bd709057
DJReTa <blackiron.old@gmail.com>
2021-07-19 00:14:36 +0200

    dodo sam edit i delete na homepage

--- src/components/ObjavaComponent.js
+++ src/components/ObjavaComponent.js
@@ -10,9 +10,19 @@ import {
 } from 'react-bootstrap-icons';
 import ObjaviKomentarComponent from "./ObjaviKomentarComponent";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 function ObjavaComponent({objava,handler,unistiSesiju,sesija}){
+    const [show, setShow] = useState(false);
+    const [show1,setShow1]=useState(false);
+    const [id,setId]=useState(0)
     const[prikazi,setPrikazi] = useState(false);
+
+
+    const handleClose = () => setShow(false);
+    const handleClose1 = () => setShow1(false);
+    const handleShow = (id) =>{setId(id); setShow(true);}
+    const handleShow1 = () => {setShow1(true);}
 
     function postaviPrikaz(){
         setPrikazi(!prikazi);
@@ -60,25 +70,60 @@ function ObjavaComponent({objava,handler,unistiSesiju,sesija}){
         });
     }
 
+    function obrisiObjavu(){
+        console.log(id)
+        setShow(false);
+        axios.post('https://dwsproject.herokuapp.com/deletePost',{
+            post_id: id
+        })
+            .then((response)=>{
+                if(!response.data.success){
+                    console.log(response.data)
+                }else{
+                    Swal.fire({
+                        title: 'Super!',
+                        text: 'Uspješno ste izbrisali objavu.',
+                        icon: 'success',
+                        confirmButtonText: 'Nastavi dalje'
+                    })
+                    handler()
+                }
+            })
+            .catch((error)=>{
+                console.log(error)
+                switch (error.response.status) {
+                    case 403:
+                        unistiSesiju();
+                    default:
+                        console.log(error)
+                }
+            })
+    }
+
     return(
         <div className="bezpaddinga">
             <Card className={"mb-3"}>
                 <Card.Img variant="top" src={objava.post.picture} className={"d-none d-lg-block"}/>
                 <Card.Body className={"w-70"}>
-                    <div className="blockquote mb-0">
-                        <p>
-                            <a href={"/objava/"+objava.post.id} className={"objavaa"}>{objava.post.text}</a>
-                        </p>
-                        <footer className="blockquote-footer mt-2">
-                            <a href={"/profil/"+objava.post.author_id} className={"bez-dekoracije"}>{objava.post.first_name} {objava.post.last_name}</a>
-                        </footer>
+                    <Row>
+                        <Col xs={11}>
+                            <div className="blockquote mb-0">
+                                <p>
+                                    <a href={"/objava/"+objava.post.id} className={"objavaa"}>{objava.post.text}</a>
+                                </p>
+                                <footer className="blockquote-footer mt-2">
+                                    <a href={"/profil/"+objava.post.author_id} className={"bez-dekoracije"}>{objava.post.first_name} {objava.post.last_name}</a>
+                                </footer>
 
-                        <Card.Text> <ChatSquareText onClick={() => postaviPrikaz()}
-                                                    className={"pokazivac"}/> <small>{objava.comments.length}</small>
-                            {!objava.post.i_have_liked? <HandThumbsUpFill className={"palac pokazivac"}  onClick={() => lajkaj()}/> :
-                                <HandThumbsDownFill className={"palac pokazivac"} onClick={() => dislajk()}/>}  <small>{objava.post.likes} oznaka sviđa mi se</small>
-                        </Card.Text>
-                    </div>
+                                <Card.Text> <ChatSquareText onClick={() => postaviPrikaz()}
+                                                            className={"pokazivac"}/> <small>{objava.comments.length}</small>
+                                    {!objava.post.i_have_liked? <HandThumbsUpFill className={"palac pokazivac"}  onClick={() => lajkaj()}/> :
+                                        <HandThumbsDownFill className={"palac pokazivac"} onClick={() => dislajk()}/>}  <small>{objava.post.likes} oznaka sviđa mi se</small>
+                                </Card.Text>
+                            </div>
+                        </Col>
+
+                    </Row>
                 </Card.Body>
             </Card>
 
