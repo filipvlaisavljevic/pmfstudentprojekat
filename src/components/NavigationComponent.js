be7bac43bfa964666ed17b8be702ff18052311e8
DJReTa <blackiron.old@gmail.com>
2021-07-19 13:19:02 +0200

    popravljen padding za sve, ne dirati više

--- src/components/NavigationComponent.js
+++ src/components/NavigationComponent.js
@@ -29,36 +29,36 @@ function NavigationComponent({sesija,unistiSesiju,korisnik}){
     }
 
     return(
-        <Navbar bg="light" expand="lg" className={"mt-3"}>
-            <Container>
-                {redirect ? <div><Redirect to={"/pretraga/"+kveri}/></div> : null}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {!sesija ? <Nav.Link href="/login">Loguj se</Nav.Link> : <div></div>}
-                        {!sesija ? <Nav.Link href="/register">Registruj se</Nav.Link> : <div></div>}
-                        { sesija ? <NavDropdown title="Moj profil">
-                            <NavDropdown.Item href={"/mojprofil/"+korisnik.id}>Profil</NavDropdown.Item>
-                            <NavDropdown.Item href="/profil/edit">Postavke</NavDropdown.Item>
-                        </NavDropdown> : <div></div> }
-                        { sesija ? <Nav.Link href="/chat">Chat</Nav.Link> : <div></div> }
-                        { sesija ? <Nav.Link onClick={() => odlogujKorisnika()}>Logout</Nav.Link> : <div></div>}
-                    </Nav>
-                    {/*Pretraživanje*/}
-                    <Form className="d-flex" onSubmit={handleSubmit(onSubmit)}>
-                        <FormControl
-                            type="search"
-                            placeholder="Pronađi prijatelje"
-                            className="mr-2"
-                            aria-label="Search"
-                            {...register("example")}
-                        />
-                        <Button variant="primary" type={"submit"}>Pretraži</Button>
-                    </Form>
+        <Container>
+            <Navbar bg="light" expand="lg" className={"mt-3"}>
+                    {redirect ? <div><Redirect to={"/pretraga/"+kveri}/></div> : null}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            {!sesija ? <Nav.Link href="/login">Loguj se</Nav.Link> : <div></div>}
+                            {!sesija ? <Nav.Link href="/register">Registruj se</Nav.Link> : <div></div>}
+                            { sesija ? <NavDropdown title="Moj profil">
+                                <NavDropdown.Item href={"/mojprofil/"+korisnik.id}>Profil</NavDropdown.Item>
+                                <NavDropdown.Item href="/profil/edit">Postavke</NavDropdown.Item>
+                            </NavDropdown> : <div></div> }
+                            { sesija ? <Nav.Link href="/chat">Chat</Nav.Link> : <div></div> }
+                            { sesija ? <Nav.Link onClick={() => odlogujKorisnika()}>Logout</Nav.Link> : <div></div>}
+                        </Nav>
+                        {/*Pretraživanje*/}
+                        <Form className="d-flex" onSubmit={handleSubmit(onSubmit)}>
+                            <FormControl
+                                type="search"
+                                placeholder="Pronađi prijatelje"
+                                className="mr-2"
+                                aria-label="Search"
+                                {...register("example")}
+                            />
+                            <Button variant="primary" type={"submit"}>Pretraži</Button>
+                        </Form>
 
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    </Navbar.Collapse>
+            </Navbar>
+        </Container>
     );
 }
 
