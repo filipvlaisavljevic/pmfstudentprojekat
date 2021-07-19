bdc0ae33a0e77552f9c0584b549da678d275af0b
DJReTa <blackiron.old@gmail.com>
2021-07-19 14:27:06 +0200

    dodani character u nova poruka

--- src/components/NovaPorukaComponent.js
+++ src/components/NovaPorukaComponent.js
@@ -1,8 +1,43 @@
-import React from "react"
+import React, {useState} from "react"
 import {CaretDownFill} from "react-bootstrap-icons";
 import {Button, Form} from "react-bootstrap";
 
 function NovaPorukaComponent(){
+    const [duzina,setDuzina]=useState(250)
+    const [prosli,setProsli]=useState(0)
+
+    function promjenaTexta(event){
+        let input=event.target.value;
+        if(input.length>prosli) {
+            setDuzina(duzina + (prosli-input.length))
+            if(duzina+(prosli-input.length)===0){
+                document.getElementById('preostalo').classList.remove('preostalo-manje')
+                document.getElementById('preostalo').classList.remove('preostalo-poruka')
+                document.getElementById('preostalo').classList.add('preostalo-nista')
+            }
+            else if (duzina + (prosli-input.length) < 20) {
+                document.getElementById('preostalo').classList.remove('preostalo-poruka')
+                document.getElementById('preostalo').classList.remove('preostalo-nista')
+                document.getElementById('preostalo').classList.add('preostalo-manje')
+            }
+            setProsli(input.length)
+        }else{
+            setDuzina(duzina+(prosli-input.length))
+            if(duzina+(prosli-input.length)>=20){
+                document.getElementById('preostalo').classList.remove('preostalo-manje')
+                document.getElementById('preostalo').classList.remove('preostalo-nista')
+                document.getElementById('preostalo').classList.add('preostalo-poruka')
+
+            }else if(duzina+(prosli-input.length)<20){
+                document.getElementById('preostalo').classList.remove('preostalo-poruka')
+                document.getElementById('preostalo').classList.remove('preostalo-nista')
+                document.getElementById('preostalo').classList.add('preostalo-manje')
+            }
+            setProsli(input.length)
+        }
+
+    }
+
     return(
         <div className={"mt-5"}>
             <div className={"banner pt-2 pb-2 mb-4"}>
@@ -19,7 +54,12 @@ function NovaPorukaComponent(){
                     </select>
                 </Form.Group>
                 <Form.Group className="mb-3">
-                    <Form.Control as="textarea" maxLength="250" rows={3} placeholder={"Unesite sadržaj nove poruke..."}/>
+                    <Form.Control as="textarea" id="t" maxLength="250" rows={3} placeholder={"Unesite sadržaj nove poruke..."}
+                                  onChange={promjenaTexta.bind(this)}
+                    />
+                    <Form.Text className="text-muted">
+                        Preostaje Vam još <b className="preostalo-poruka" id="preostalo">{duzina}</b> karaktera.
+                    </Form.Text>
                 </Form.Group>
                 <Button type={"primary"} className={"w-100"}>Pošaljite poruku</Button>
             </Form>
