6bdcd30c4e1b441fa1e28b52bb480def98d2d97a
DJReTa <blackiron.old@gmail.com>
2021-07-19 23:26:36 +0200

    promjenio na 'Aktivan prije'

--- src/components/NjihovProfilComponent.js
+++ src/components/NjihovProfilComponent.js
@@ -84,7 +84,7 @@ function NjihovProfilComponent({unistiSesiju,sesija}){
 
     const renderNeaktivan = (props) => (
         <Tooltip id="button-tooltip" {...props}>
-            {korisnik.last_active ? 'Zadnji put online prije '+korisnik.last_active : 'Neaktivan'}
+            {korisnik.last_active ? 'Aktivan prije '+korisnik.last_active : 'Neaktivan'}
         </Tooltip>
     );
 
